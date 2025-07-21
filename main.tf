5f603ab0f0ebd62ef58967ac3d5f0a1dd895600c
thuto mothapa <thutomothapa@gmail.com>
2025-07-21 07:33:38 +0000

    changed some stuff

--- main.tf
+++ main.tf
@@ -121,6 +121,13 @@ resource "aws_security_group" "ecs" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5678
+    to_port     = 5678
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
